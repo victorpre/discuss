94ca69d89a8c9bcefe1253072fac4407e19cb6bd
Victor Presumido <victor.presumido@gmail.com>
2018-09-26 16:51:37 +0200

    Finish topic creation workflow

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -4,9 +4,24 @@ defmodule DiscussWeb.TopicController do
   alias Discuss.Forum
   alias Discuss.Forum.Topic
 
-  def new(conn, params) do
+  def new(conn, _params) do
     changeset = Forum.change_topic(%Topic{})
 
     render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic_params}) do
+    case Forum.create_topic(topic_params) do
+      {:ok, topic} ->
+        conn
+        |> put_flash(:info, "Topic created")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} -> render conn, "new.html", changeset: changeset
+    end
+  end
+
+  def index(conn, _params) do
+    topics = Forum.all_topics
+    render conn, "index.html", topics: topics
   end
 end
